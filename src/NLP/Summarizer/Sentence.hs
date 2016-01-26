1ae9cd7333c591684b112a2725fd3ba4548bec01
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2016-01-26 02:52:29 -0300

    Ported Sentence module
    Port: renamed Sentence words field to terms
    
    Port: Sentence fields should use Text datatype

---
+++ src/NLP/Summarizer/Sentence.hs
@@ -0,0 +1,13 @@
+module NLP.Summarizer.Sentence where
+
+import NLP.Summarizer.Term
+
+import Data.Text (Text)
+
+data Sentence =
+  Sentence { terms :: [Term]
+           , score :: Double
+           , selected :: Bool
+           , wordCount :: Int
+           , originalSentence :: Text
+           }
